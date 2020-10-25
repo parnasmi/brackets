623852d9340922f414b4851186f35f6c1d997bae
Ilhom <parnasmi@gmail.com>
2020-10-25 22:06:09 +0500

    init: implement brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,79 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let inputStack = str.split('');
+  let bracketsStack = [];
+  while (inputStack.length > 0) {
+    if (checkDouble(bracketsConfig, topStack(inputStack))) {
+      if (bracketsStack.length > 0) {
+        if (bracketsStack.some(brackets => brackets == topStack(inputStack))){
+          if (topStack(inputStack) == topStack(bracketsStack)) {
+            inputStack.pop();
+            bracketsStack.pop();
+          }
+          else {
+            return false;
+          }
+        }
+        else {
+          bracketsStack.push(inputStack.pop());
+        }
+      }
+      else {
+        bracketsStack.push(inputStack.pop());
+      }
+    }
+    else {
+      if (checkOpen(bracketsConfig, (topStack(inputStack)))) {
+        if (bracketsStack.length > 0) {
+          if (checkClose(bracketsConfig, topStack(inputStack)) == topStack(bracketsStack)) {
+            inputStack.pop();
+            bracketsStack.pop();
+          }
+          else {
+            return false;
+          }
+        }
+        else { return false }
+      }
+      else {
+        bracketsStack.push(inputStack.pop());
+      }
+    }
+  }
+  if (inputStack.length == 0 && bracketsStack.length == 0) {
+    return true
+  }
+  else { return false; }
+}
+function topStack(stack) {
+  return stack[stack.length - 1];
+}
+function checkOpen(arrayConfig, str) {
+  for (let i = 0; i <= arrayConfig.length - 1; i++) {
+    if (arrayConfig[i][0] == str) {
+      a = true;
+      break;
+    }
+    else { a = false }
+  }
+  return a;
+}
+function checkClose(arrayConfig, str) {
+  for (let i = 0; i <= arrayConfig.length - 1; i++) {
+    if (arrayConfig[i][0] == str) {
+      a = arrayConfig[i][1];
+      break;
+    }
+    else { a = false }
+  }
+  return a;
+}
+function checkDouble(arrayConfig, str) {
+  for (let i = 0; i <= arrayConfig.length - 1; i++) {
+    if (arrayConfig[i][0] == str && arrayConfig[i][1] == str) {
+      a = true;
+      break;
+    }
+    else { a = false }
+  }
+  return a;
 }
